a5792e85a7e165cd24552f000831f7715d1edbb8
TruongPhanDinhTruc <truongphandinhtruc@gmail.com>
2024-09-29 00:40:01 +0700

    redux equManagement

---
+++ src/page/EquipManagement/equManagement.js
@@ -0,0 +1,241 @@
+import { CaretDownOutlined, FilterOutlined, SortAscendingOutlined } from '@ant-design/icons'
+import { Button, Dropdown, Form, Space, Tag } from 'antd'
+import Search from 'antd/es/transfer/search'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { realtimeDB } from "../../firebase";
+import { onValue, ref } from 'firebase/database';
+import { setPageTitle } from "../../redux/page/pageSlice";
+import { Menu, MenuHandler, MenuItem, MenuList } from '@material-tailwind/react';
+import EquTable from './equTable';
+
+function Equ() {
+    const locations = useSelector(
+        (state) => state.location?.location?.allLocation
+    );
+    const [form] = Form.useForm();
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isAddModal, setIsAddModal] = useState("");
+    const [searchText, setSearchText] = useState("");
+    const [sortType, setSortType] = useState("");
+    const [filterType, setFilterType] = useState({ type: null, value: "", name: "" });
+    const [locationFilterMenu, setLocationFilterMenu] = useState([]);
+    const [isOpenStatusFilter, setIsOpenStatusFilter] = useState(false);
+    const [isOpenLocationFilter, setIsOpenLocationFilter] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!sessionStorage.getItem("admin")) {
+            navigate("/auth");
+            return;
+        }
+        dispatch(setPageTitle("Equip Management"));
+        getLocation();
+    }, []);
+
+    const getLocation = () => {
+        if (!locations) {
+            const locationRef = ref(realtimeDB, "location");
+            onValue(
+                locationRef,
+                (snapshot) => {
+                    if (snapshot.exists()) {
+                        const locationList = snapshot.val();
+                        setLocationFilterMenu(locationList);
+                        // dispatch(getLocations(locationList));
+                    }
+                },
+                {
+                    onlyOnce: true,
+                }
+            );
+            return;
+        }
+        return setLocationFilterMenu(locations);
+    };
+
+    const itemsSort = [
+        {
+            label: "Name", key: "1",
+            children: [
+                { label: "A to Z", key: "aToZ", },
+                { label: "Z to A", key: "zToA", },
+            ],
+        },
+        // { label: "Location", key: "2", },
+        {
+            label: "GPA", key: "3",
+            children: [
+                { label: "Ascending", key: "low2High", },
+                { label: "Descending", key: "high2Low", },
+            ],
+        },
+    ];
+
+    const locationMenuList = (location) => (
+        <MenuItem
+            key={location.id}
+            onClick={(e) => handleFilter("location", e.target.value, location.locationName)}
+            value={location.id}
+        >
+            {location.locationName}
+        </MenuItem>
+    );
+    const locationMap = locationFilterMenu.map(locationMenuList);
+
+    const handleFilter = (type, e, name) => {
+        setFilterType({
+            type: type,
+            value: e,
+            name: name
+        });
+        setCurrentPage(1);
+    }
+    // console.log("Data std: ", form);
+    // console.log("Filter : ", filterType);
+
+    return (
+        <>
+            <div className='p-2 mt-8 flex items-center bg-white shadow-bottom dark:bg-white'>
+                <Button
+                    className="ml-2 mr-2 bg-purple"
+                    size="large"
+                    onClick={() => {
+                        setIsAddModal(true);
+                        setIsOpenModal(true);
+                    }}>
+                    Add Equip
+                </Button>
+                <div className="w-1/3 justify-start">
+                    <Search
+                        className="ml-2 "
+                        placeholder="Search by student name, GPA..."
+                        allowClear
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                            setCurrentPage(1)
+                        }}
+                        size="large" />
+                </div>
+                <div className="w-2/3 flex justify-end">
+                    {filterType.type === "status" && (
+                        <Tag
+                            className="items-center flex"
+                            color="blue"
+                            closable
+                            onClose={() => handleFilter(null, "", "")}>
+                            Filter by {filterType.value}
+                        </Tag>
+                    )}
+
+                    {filterType.type === "location" && (
+                        <Tag
+                            className="items-center flex"
+                            color="blue"
+                            closable
+                            onClose={() => handleFilter(null, "", "")}>
+                            Filter by {filterType.name}
+                        </Tag>
+                    )}
+
+                    {sortType && (
+                        <Tag
+                            className="items-center flex"
+                            color="blue"
+                            closable
+                            onClose={() => setSortType("")}>
+                            Sort by {sortType === "low2High" || sortType === "high2Low" ? "GPA" : "name"}
+                        </Tag>
+                    )}
+
+                    {/* <Menu allowHover>
+                        <MenuHandler>
+                            <Button size="large" className="bg-purple">
+                                Filter
+                                <FilterOutlined></FilterOutlined>
+                            </Button>
+                        </MenuHandler>
+                        <MenuList>
+                            <Menu
+                                placement="right-start"
+                                allowHover
+                                open={isOpenLocationFilter}
+                                handler={setIsOpenLocationFilter}
+                                offset={15}>
+                                <MenuHandler className="flex items-center justify-between">
+                                    <MenuItem>
+                                        Location
+                                        <CaretDownOutlined
+                                            strokeWidth={2.5}
+                                            className={`h-3.5 w-3.5 transition-transform ${isOpenLocationFilter ? "-rotate-90" : ""
+                                                }`} />
+                                    </MenuItem>
+                                </MenuHandler>
+                                <MenuList className="max-h-72">{locationMap}</MenuList>
+                            </Menu>
+                            <Menu
+                                placement="right-start"
+                                open={isOpenStatusFilter}
+                                handler={setIsOpenStatusFilter}
+                                allowHover
+                                offset={15}
+                            >
+                                <MenuHandler className="flex items-center justify-between">
+                                    <MenuItem>
+                                        Status
+                                        <CaretDownOutlined
+                                            strokeWidth={2.5}
+                                            className={`h-3.5 w-3.5 transition-transform ${isOpenStatusFilter ? "-rotate-90" : ""
+                                                }`}
+                                        />
+                                    </MenuItem>
+                                </MenuHandler>
+                                <MenuList onClick={(e) => handleFilter("status", e.target.value, "")}>
+                                    <MenuItem value={"approve"}>Approved</MenuItem>
+                                    <MenuItem value={"requesting"}>Requesting</MenuItem>
+                                    <MenuItem value={"rejected"}>Rejected</MenuItem>
+                                </MenuList>
+                            </Menu>
+                        </MenuList>
+                    </Menu> */}
+                    <Dropdown
+                        className="ml-2 bg-purple"
+                        menu={{
+                            items: itemsSort,
+                            onClick: ({ key }) => setSortType(key),
+                        }}>
+                        <Button size="large">
+                            <Space>
+                                Sort
+                                <SortAscendingOutlined />
+                            </Space>
+                        </Button>
+                    </Dropdown>
+
+
+                </div>
+            </div>
+
+            <EquTable
+                form={form}
+                setIsAddModal={setIsAddModal}
+                setIsOpenModal={setIsOpenModal}
+                searchText={searchText}
+                sortType={sortType}
+                filterType={filterType}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
+
+            {isOpenModal && (
+                <></>
+            )}
+        </>
+    )
+}
+
+export default Equ
---
+++ src/page/EquipManagement/equTable.js
@@ -0,0 +1,199 @@
+import { CheckOutlined, CloseOutlined, DeleteFilled } from "@ant-design/icons";
+import { Pagination, Space, Switch, Table, Tag, Modal, } from "antd";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { realtimeDB } from "../../firebase";
+import { onValue, ref, remove, update } from "firebase/database";
+import { toast } from "react-toastify";
+
+function EquTable({ form, setIsAddModal, setIsOpenModal, searchText, sortType, filterType, currentPage, setCurrentPage }) {
+    const [equList, setEquList] = useState([]);
+    const [pageSize, setPageSize] = useState(10);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getAllEquFormDb();
+    }, []);
+
+    const getAllEquFormDb = () => {
+        const supRef = ref(realtimeDB, "equ");
+        onValue(supRef, (snapshot) => {
+            if (snapshot.exists()) {
+                const equList = snapshot.val();
+                setEquList(equList);
+                // dispatch(getAllStd(stdList));
+            }
+        });
+    };
+
+    const handlePaging = (current, pageSize) => {
+        setCurrentPage(current);
+        setPageSize(pageSize);
+    };
+
+    const handleRowClick = (data) => {
+        form.setFieldsValue(data);
+        setIsAddModal(false);
+        setIsOpenModal(true);
+    };
+    const rowClassName = (record) => {
+        return record.stdStatus === "2"
+            ? "text-gray-400 even:bg-gray-100"
+            : "even:bg-gray-100";
+    };
+
+    const sortList = (stdList, order) => {
+        return [...stdList].sort((a, b) => {
+            if (order === "low2High") return a.stdGPA - b.stdGPA;
+            if (order === "high2Low") return b.stdGPA - a.stdGPA;
+            if (order === "aToZ") return a.equName.localeCompare(b.equName);
+            if (order === "zToA") return b.equName.localeCompare(a.equName);
+            return 0;
+        });
+    };
+
+    const filterByStatus = (stdList, status) => {
+        if (status.type !== "status")
+            return stdList;
+        switch (status.value) {
+            case "approve":
+                return stdList.filter((std) => std.stdStatus === "1");
+            case "requesting":
+                return stdList.filter((std) => std.stdStatus === "3");
+            case "rejected":
+                return stdList.filter((std) => std.stdStatus === "2");
+            default:
+                return stdList;
+        }
+    };
+
+    const filterByLocation = (stdList, locationFilter) => {
+        if (locationFilter.type !== "location")
+            return stdList;
+        return stdList = stdList.filter((std) => std.stdHighSchoolLocation === String(locationFilter.value))
+    };
+
+    const equSearchList = equList.filter(
+        (equ) =>
+            equ.equName?.toLowerCase().includes(searchText?.toLowerCase())
+    );
+    const equFilterLocation = filterByLocation(equSearchList, filterType);
+    const equFilterList = filterByStatus(equFilterLocation, filterType);
+    const equSortList = sortList(equFilterList, sortType);
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const stdListSlice = equSortList.slice(startIndex, endIndex);
+    // console.log("Student List: ", stdListFromRedux);
+    // console.log("Student List search: ", stdSearchList);
+    // console.log("Student List location: ", stdFilterLocation);
+    // console.log("Student List status: ", stdFilterList);
+    // console.log("Student List sort: ", stdSortList);
+
+    const confirmSwitchStatus = (record, newStdStatus, newUserStatus, event) => {
+        event.stopPropagation();
+        Modal.confirm({
+            title: "Are you sure?",
+            content: "Do you want to change the status?",
+            onOk() {
+                const recordRef = ref(realtimeDB, `std/${record.id}`);
+                update(recordRef, {
+                    stdStatus: newStdStatus,
+                    userStatus: newUserStatus,
+                });
+                getAllEquFormDb();
+                toast.success("Change students status success.");
+
+            },
+        });
+    };
+
+    const confirmDelete = (record, event) => {
+        event.stopPropagation();
+        Modal.confirm({
+            title: "Are you sure?",
+            content: "Do you want to delete this student?",
+            onOk() {
+                const recordRef = ref(realtimeDB, `std/${record.id}`);
+                remove(recordRef);
+                getAllEquFormDb();
+                toast.success("Remove student success.");
+            },
+        });
+    };
+
+    const columns = [
+        {
+            title: "No",
+            dataIndex: "id",
+            key: "id",
+            render: (text) => <span>{text}</span>,
+        },
+        {
+            title: "Name",
+            dataIndex: "equName",
+            key: "equName",
+        },
+        {
+            title: "Quantily",
+            dataIndex: "equQuantily",
+            key: "equQuantily",
+        },
+        {
+            title: "Manufacture Date",
+            dataIndex: "equManufactureDate",
+            key: "equManufactureDate",
+        },
+        {
+            title: "Price",
+            dataIndex: "equPrice",
+            key: "equPrice",
+        },
+        {
+            title: "Expiry Date",
+            dataIndex: "equExpiryDate",
+            key: "equExpiryDate",
+        },
+        {
+            title: "Manufacturer",
+            dataIndex: "equManufacturer",
+            key: "equManufacturer",
+        },
+        {
+            title: "Status",
+            dataIndex: "equStatus",
+            key: "equStatus",
+        },
+    ];
+    return (
+        <>
+            <Table
+                className="py-4"
+                pagination={false}
+                columns={columns}
+                dataSource={stdListSlice}
+                rowClassName={rowClassName}
+                onRow={(record, rowIndex) => {
+                    return {
+                        onClick: (event) => {
+                            handleRowClick(record);
+                        }, // click row
+                    };
+                }}
+            />
+            <div className="flex justify-center">
+                <Pagination
+                    total={equSortList?.length}
+                    showSizeChanger
+                    showQuickJumper
+                    current={currentPage}
+                    pageSize={pageSize}
+                    showTotal={(total) => `Total ${total} students`}
+                    onChange={handlePaging}
+                ></Pagination>
+            </div>
+        </>
+    );
+}
+
+export default EquTable;
